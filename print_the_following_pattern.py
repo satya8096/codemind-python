3c6a10b1f7a22dbd527ed4aed3f26de1318fc1a5
satya8096 <112757107+satya8096@users.noreply.github.com>
2022-11-06 11:47:02 +0530

    a=int(input()) r=0 for i in range(1,a+1):     rem=i%10     r=r*10+rem     i=i//10     print(r)

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,7 @@
 a=int(input())
-for i in range(a):
-    for j in range(a,0,-1):
-        print(j,end= ' ')
-    print()
+r=0
+for i in range(1,a+1):
+    rem=i%10
+    r=r*10+rem
+    i=i//10
+    print(r)
