347cba55a1317b85f607fd9ee02a0a262f6b09dc
satya8096 <112757107+satya8096@users.noreply.github.com>
2022-11-08 15:41:35 +0530

    def isprime(c):     for i in range(c+1,100000):         c1=0         for j in range(1,i+1):             if i%j==0:                 c1+=1         if c1==2:             return i             break a=int(input()) b=int(input()) c=a+b print(isprime(c)-c)

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -1,10 +1,10 @@
-def isprime(a):
-    for i in range(a+1,100000):
-        c=0
+def isprime(c):
+    for i in range(c+1,100000):
+        c1=0
         for j in range(1,i+1):
             if i%j==0:
-                c+=1
-        if c==2:
+                c1+=1
+        if c1==2:
             return i
             break
 a=int(input())
